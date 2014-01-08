670bcdb66e6d9a18d9becf30a663d2e287ce27f9
Jacob Burenstam <burenstam@gmail.com>
2014-01-08 17:03:16 +0100

    Refactor

--- src/git_story.sh
+++ src/git_story.sh
@@ -10,7 +10,7 @@ function gs {
   elif [[ $1 == "help" ]]; then
     __gs_functions "$2" "-help"
   else
-    __gs_functions "$1" "$2"
+    __gs_functions "$@"
   fi
 }
 
@@ -61,17 +61,30 @@ function __gs-dev {
     echo "You must provide a branch name"
     echo "Missing argument <branch_name>"
     __gs-dev-help
+    return
+  fi
+  if [[ $1 == "-help" ]] || [[ $1 == "--help" ]]; then
+    __gs-dev-help
+    return
+  fi
+
+  if [[ $(git status 2> /dev/null | tail -n1) != *"working directory clean"* ]]; then
+    __uncommitted-changes-message
   else
-    if [[ $1 == "-help" ]]; then
-      __gs-dev-help
-    else
-      local branch=${2-master}
-      git checkout $branch
+    base=$2
+    branch=${base:-master}
+    echo "Base branch: $branch"
+    git checkout $branch
+    if git show-ref --verify --quiet "refs/heads/$branch";then
+      echo "Updating $branch, from repository."
       git pull origin $branch
-      git checkout -b $1
-      git branch
-      echo -e "You're now ready to implement your feature"
     fi
+    echo "Creating branch: $1"
+    git checkout -b $1
+    git branch
+    echo -e "You're now ready to implement your feature"
+    echo "Based of: $branch branch"
+    echo "Current branch $1"
   fi
 }
 
@@ -83,8 +96,12 @@ function __gs-update-help {
 }
 
 function __gs-update {
-  if [[ $1 == "-help" ]]; then
+  if [[ $1 == "-help" ]] || [[ $1 == "--help" ]]; then
       __gs-update-help
+    return
+  fi
+  if [[ $(git status 2> /dev/null | tail -n1) != *"working directory clean"* ]]; then
+    __uncommitted-changes-message
   else
     git pull origin master
   fi
@@ -99,20 +116,27 @@ function __gs-checkpoint-help {
 function __gs-checkpoint {
   if [[ -z "$1" ]]; then
     echo "You must provide a commit message"
-    echo "Missing argument <'Commit message'>"
+    echo "Missing argument 'Commit message'"
+    __gs-checkpoint-help
+    return
+  fi
+  if [[ $1 == "-help" ]] || [[ $1 == "--help" ]]; then
     __gs-checkpoint-help
   else
-    if [[ $1 == "-help" ]]; then
-      __gs-checkpoint-help
-    else
+    if [[ $(git status 2> /dev/null | tail -n1) != *"working directory clean"* ]]; then
       git add --all
       git commit -m "$1"
+    else
+      echo "No changes to commit."
     fi
   fi
 }
 
 function __gs-ready-help {
   echo -e "usage: "
+  echo -e "\t gs done"
+  echo "only if all changes have been committed."
+  echo "otherwise run:"
   echo -e "\t gs done 'Commit message'"
   echo -e "commit message example: "
   echo -e "\t 'Implemented story 13."
@@ -124,53 +148,53 @@ function __gs-ready-help {
 }
 
 function __gs-ready {
-  if [[ $1 == "-help" ]]; then
+  if [[ $1 == "-help" ]] || [[ $1 == "--help" ]]; then
       __gs-ready-help
-  else
-      __gs-ready-checklist-print
-    while true; do
-      read -p "Have you answered yes to all of the above? (y\n)" yn
-      case $yn in
-        [Yy]* ) __gs-ready-execute "$1"; break;;
-        [Nn]* ) break;;
-        * ) echo "Please answer yes or no.";;
-      esac
-    done
+    return
   fi
+  __gs-ready-checklist-print
+  while true; do
+    read -p "Have you answered yes to all of the above? (y\n)" yn
+    case $yn in
+      [Yy]* ) __gs-ready-execute "$1"; break;;
+      [Nn]* ) break;;
+      * ) echo "Please answer yes or no.";;
+    esac
+  done
 }
 
 function __gs-ready-execute {
-  if [[ -z "$1" ]]; then
-    echo "You must provide a commit message!"
-    __gs-ready-help
-  else
+  if [[ $(git status 2> /dev/null | tail -n1) != *"working directory clean"* ]]; then
+    if [[ -z "$1" ]]; then
+      echo "You have uncommited changes you must provide a commit message."
+      __gs-ready-help
+      return
+    fi
     git add --all
     git commit -m "$1"
-    local BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-    git push origin $BRANCH
-    git checkout master
-    git pull origin master
-    git merge $BRANCH
-    echo -e "If any merge conflicts fix them and run:"
-    echo -e "\t gs checkpoint 'message'"
-    echo -e "If no conflicts:"
-    echo -e "\t gs release"
   fi
+  local BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+  git push origin $BRANCH
+  git pull origin master
+  echo -e "If any merge conflicts fix them and then run:"
+  echo -e "\t gs checkpoint 'message'"
+  echo -e "If no conflicts:"
+  echo -e "\t gs release"
 }
 
 function __gs-release {
-  if [[ $1 == "-help" ]]; then
-      __gs-release-help
-  else
-    while true; do
-      read -p "Are you sure you want to release? (y\n)" yn
-      case $yn in
-        [Yy]* ) __gs-release-execute; break;;
-        [Nn]* ) break;;
-        * ) echo "Please answer yes or no.";;
-      esac
-    done
+  if [[ $1 == "-help" ]] || [[ $1 == "--help" ]]; then
+     __gs-release-help
+     return
   fi
+  while true; do
+    read -p "Are you sure you want to release? (y\n)" yn
+    case $yn in
+      [Yy]* ) __gs-release-execute; break;;
+      [Nn]* ) break;;
+      * ) echo "Please answer yes or no.";;
+    esac
+  done
 }
 
 function __gs-release-help {
@@ -201,11 +225,11 @@ function __gs-diff-help {
 }
 
 function __gs-diff {
-  if [[ $1 == "-help" ]]; then
+  if [[ $1 == "-help" ]] || [[ $1 == "--help" ]]; then
     __gs-diff-help
-  else
-    git diff
+    return
   fi
+  git diff
 }
 
 function __gs-switchto-help {
@@ -215,11 +239,11 @@ function __gs-switchto-help {
 }
 
 function __gs-switchto {
-  if [[ $1 == "-help" ]]; then
+  if [[ $1 == "-help" ]] || [[ $1 == "--help" ]]; then
     __gs-switchto-help
-  else
-    git checkout $1
+    return
   fi
+  git checkout $1
 }
 
 function __gs-history-help {
@@ -229,11 +253,11 @@ function __gs-history-help {
 }
 
 function __gs-history {
-  if [[ $1 == "-help" ]]; then
+  if [[ $1 == "-help" ]] || [[ $1 == "--help" ]]; then
     __gs-history-help
-  else
-    git log $1
+    return
   fi
+  git log $1
 }
 
 function __gs-show-help {
@@ -245,11 +269,11 @@ function __gs-show-help {
 }
 
 function __gs-show {
-  if [[ $1 == "-help" ]]; then
+  if [[ $1 == "-help" ]] || [[ $1 == "--help" ]]; then
     __gs-show-help
-  else
-    git show $1
+    return
   fi
+  git show $1
 }
 
 function __gs-where-help {
@@ -259,11 +283,18 @@ function __gs-where-help {
 }
 
 function __gs-where {
-  if [[ $1 == "-help" ]]; then
+  if [[ $1 == "-help" ]] || [[ $1 == "--help" ]]; then
     __gs-where-help
-  else
-    git branch
+    return
   fi
+  git branch
+}
+
+function __uncommitted-changes-message {
+  echo -e "You have uncommited changes."
+  echo -e "Please commit or stash you're changes before implementing you're new feature."
+  echo "Perhaps create a checkpoint:"
+  __gs-checkpoint-help
 }
 
 
@@ -275,7 +306,7 @@ function __gs-help {
   echo -e "Run any command followed by 'help' for command details."
   __gs-list-commands
   echo -e "help: "
-  echo -e "\t gs 'command' help"
+  echo -e "\t gs 'command' -help"
   echo -e "usage: "
   echo -e "\t gs dev story23"
   echo -e "\t gs done 'Implemented Story 23'"
