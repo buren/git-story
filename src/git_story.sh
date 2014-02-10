7887fe0d9f4bec484673b5114d23385f14cf49e4
Jacob Burenstam <burenstam@gmail.com>
2014-02-10 08:53:53 +0100

    Added stash ability for gs

--- src/git_story.sh
+++ src/git_story.sh
@@ -52,6 +52,8 @@ __gs_functions() {
     __gs-status "$2"
   elif [[ $1 == "where" ]] || [[ $1 == "branches" ]]; then
     __gs-where "$2"
+  elif [[ $1 == "stash" ]]; then
+    __gs-stash "$2"
   elif [[ $1 == "stat" ]] || [[ $1 == "statistics" ]]; then
     __gs-stat "$2" "$3"
   elif [[ $1 == "get_update" ]] || [[ $1 == "get-update" ]] || [[ $1 == "getupdate" ]]; then
@@ -667,6 +669,56 @@ __gs-where() {
     __gs-ignore-args "gs where"
   fi
   git branch
+}
+
+__gs-stash-help() {
+    __gs-print "
+usage:
+\t gs stash <arg>
+\t          push          # Save all changes to stash (alias: save)
+\t          apply         # Apply all changes from latest stash (alias: pop)
+\t          list          # List stash stack
+\t          show          # Show the latest stash
+\t          clear         # Clears the entire stash stack
+Manage git-stash stack."
+}
+
+__gs-stash() {
+  if [[ -z $1 ]]; then
+    __gs-error "Needs argument."
+    return
+  elif [[ $1 == "-help" ]] || [[ $1 == "--help" ]]; then
+    __gs-stash-help
+    return
+  fi
+
+  if [[ $1 == "push" ]] || [[ $1 == "save" ]]; then
+    git stash save --all
+  elif [[ $1 == "apply" ]] || [[ $1 == "pop" ]]; then
+    git stash apply
+  elif [[ $1 == "list" ]]; then
+    git stash list
+  elif [[ $1 == "show" ]]; then
+    git stash show
+  elif [[ $1 == "clear" ]]; then
+    confirm_message='Are you sure you want to clear your entire stash?'
+    while true; do
+      if [[ $SHELL == "/bin/zsh" ]]; then
+        yn=""
+        vared -p "$confirm_message (y\n)" yn
+      else
+        read -p "$confirm_message  (y\n)" yn
+      fi
+      case $yn in
+        [Yy]* ) git stash clear || __gs-error "Failed"; break;;
+        [Nn]* ) __gs-warning "Aborted."; break;;
+        * ) echo "Please answer yes or no.";;
+      esac
+    done
+    return
+  else
+    __gs-error "Unknown command '$1'"
+  fi
 }
 
 __gs-uncommitted-changes-message() {
